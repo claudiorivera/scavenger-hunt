81ce2846caad5a977b2095791629e4ea28010593
Claudio Rivera <me@claudiorivera.com>
2020-10-20 18:09:39 -0500

    Collect page only shows non-collected items for the current user

--- pages/collect.js
+++ pages/collect.js
@@ -1,11 +1,30 @@
-import { Button, Container, Typography } from "@material-ui/core";
+import { Button, Container, styled, Typography } from "@material-ui/core";
 import middleware from "@middleware";
 import Item from "@models/Item";
 import { getSession, signIn, useSession } from "next-auth/client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const StyledButton = styled(Button)({
+  margin: ".5rem .25rem",
+});
 
 const CollectPage = ({ items }) => {
   const [session] = useSession();
+  const [item, setItem] = useState(items[0]);
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+  const router = useRouter();
+
+  // Adapted from https://medium.com/swlh/simple-react-app-with-context-and-functional-components-a374b7fb66b5
+  useEffect(() => {
+    setItem({ ...items[currentItemIndex] });
+  }, [currentItemIndex]);
+
+  const getNextItem = () => {
+    let index = currentItemIndex;
+    index === items.length - 1 ? (index = 0) : index++;
+    setCurrentItemIndex(index);
+  };
 
   if (!session)
     return (
@@ -14,7 +33,6 @@ const CollectPage = ({ items }) => {
           You must be logged in to view this page.
         </Typography>
         <Button
-          type="submit"
           size="large"
           fullWidth
           color="secondary"
@@ -26,15 +44,38 @@ const CollectPage = ({ items }) => {
       </Container>
     );
 
-  return (
+  return items.length ? (
     <Container align="center">
-      <Typography variant="h3">Find...</Typography>
-      {items &&
-        items.map((item) => (
-          <Typography key={item._id} variant="body1">
-            {item.itemDescription}
-          </Typography>
-        ))}
+      <Typography variant="h1">Find</Typography>
+      <Typography variant="h2">{item.itemDescription}</Typography>
+      <Button
+        size="large"
+        fullWidth
+        color="secondary"
+        variant="contained"
+        onClick={() => {
+          getNextItem();
+        }}
+      >
+        Skip
+      </Button>
+    </Container>
+  ) : (
+    <Container align="center">
+      <Typography variant="h1">You Found All The Items!</Typography>
+      <Container>
+        <StyledButton
+          size="large"
+          fullWidth
+          color="secondary"
+          variant="contained"
+          onClick={() => {
+            router.push(`/collections/${session.user.id}`);
+          }}
+        >
+          My Collection
+        </StyledButton>
+      </Container>
     </Container>
   );
 };
@@ -46,15 +87,23 @@ export const getServerSideProps = async ({ req, res }) => {
   if (session) {
     await middleware.apply(req, res);
     const items = await Item.find().lean();
+    // TODO: Refactor this so that it's not O(n^2)
+    // Once CollectionItems are implemented, it will be a faster lookup
+    const itemsNotFoundByThisUser = items.filter(
+      (item) =>
+        item.usersWithItemCollected.filter(
+          (user) => user._id.toString() === session.user.id
+        ).length === 0
+    );
     return {
       props: {
-        items: JSON.parse(JSON.stringify(items)),
+        items: JSON.parse(JSON.stringify(itemsNotFoundByThisUser)),
       },
     };
   } else
     return {
       props: {
-        items: null,
+        items: [],
       },
     };
 };
