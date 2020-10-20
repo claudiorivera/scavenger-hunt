63ad4a2d07ea12b0ada16f002d3b61edd4f9cf86
Claudio Rivera <me@claudiorivera.com>
2020-10-20 11:49:03 -0500

    Stripped down to dummy version

--- README.md
+++ README.md
@@ -1,36 +1,3 @@
 # scavenger-hunt
 
-A game that challenges you to find the most random items around your house. Made for a virtual party our friends threw.
-
-# Technologies Used
-
-- React
-- [Next.js](https://nextjs.org)
-- Serverless API Routes
-- Material-UI
-- MongoDB
-- [NextAuth.js](https://next-auth.js.org) for GitHub OAuth and "Magic Link"
-
-# New in This Version
-
-- Initial release
-
-# Future Improvements
-
-- Initial release
-
-# Install
-
-`yarn`
-
-# Config
-
-- Add a `.env.local` file with environmental variables as shown in the example `.sample-env.local` file. You can also add these on Vercel and do `vercel pull .env.local` on your machine, which saves you a step.
-
-# Dev
-
-`yarn dev`
-
-# Known Bugs
-
-- None yet.
+Coming soon...
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,5 @@
-import { Button, Container, Typography } from "@material-ui/core";
-import { signIn, useSession } from "next-auth/client";
 import React from "react";
 
-const HomePage = () => {
-  const [session] = useSession();
-
-  if (!session)
-    return (
-      <Container>
-        <Typography variant="h5" align="center">
-          Welcome. Please login.
-        </Typography>
-        <Button
-          type="submit"
-          size="large"
-          fullWidth
-          color="secondary"
-          variant="contained"
-          onClick={signIn}
-        >
-          Login
-        </Button>
-      </Container>
-    );
-
-  return (
-    <Container>
-      <Typography variant="body1">Hello, {session.user.name}.</Typography>
-    </Container>
-  );
-};
+const HomePage = () => <h1>Coming soon...</h1>;
 
 export default HomePage;
