41c4bac9a0bb442432adef30ee9caa31fa728f44
Claudio Rivera <me@claudiorivera.com>
2020-10-19 23:06:39 -0500

    Populate default values for new users

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import Providers from "next-auth/providers";
 import nextConnect from "next-connect";
 import middleware from "../../../middleware";
 import verificationRequest from "../../../util/verificationRequest";
+import User from "../../../models/User";
 
 const handler = nextConnect();
 
@@ -10,7 +11,6 @@ handler.use(middleware);
 
 handler.use((req, res) =>
   NextAuth(req, res, {
-    debug: true,
     providers: [
       Providers.GitHub({
         clientId: process.env.GITHUB_CLIENT_ID,
@@ -33,16 +33,23 @@ handler.use((req, res) =>
       jwt: true,
     },
     callbacks: {
-      jwt: async (token, user) => {
+      jwt: async (token, user, _account, _profile, isNewUser) => {
+        if (isNewUser) {
+          try {
+            // Find the user and save, so that missing values populate with defaults
+            const userFound = await User.findById(user.id);
+            await userFound.save();
+          } catch (error) {
+            console.log(error);
+          }
+        }
         if (user) {
           token.uid = user.id;
-          token.emailVerified = user.emailVerified;
         }
         return Promise.resolve(token);
       },
       session: async (session, token) => {
         session.user.id = token.uid;
-        session.user.emailVerified = token.emailVerified;
         return Promise.resolve(session);
       },
     },
