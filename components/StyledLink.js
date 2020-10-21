ce73bed10d4820001b86efec393d49fb119b9dde
Claudio Rivera <me@claudiorivera.com>
2020-10-21 15:30:31 -0500

    Remove underline from styled links

--- components/StyledLink.js
+++ components/StyledLink.js
@@ -2,6 +2,9 @@ import { Link, styled } from "@material-ui/core";
 
 const StyledLink = styled(Link)({
   cursor: "pointer",
+  "&:hover": {
+    textDecoration: "none",
+  },
 });
 
 export default StyledLink;
