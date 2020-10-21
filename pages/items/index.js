27384c05e9cc9d925236ee60301ab24b0f907160
Claudio Rivera <me@claudiorivera.com>
2020-10-21 10:37:38 -0500

    Items page shows all items and indicates found ones

--- pages/items/index.js
+++ pages/items/index.js
@@ -1,19 +1,68 @@
 import NotLoggedInMessage from "@components/NotLoggedInMessage";
 import StyledButton from "@components/StyledButton";
 import { Container, Typography } from "@material-ui/core";
-import { signIn, useSession } from "next-auth/client";
+import middleware from "@middleware";
+import Item from "@models/Item";
+import { getSession, useSession } from "next-auth/client";
+import Link from "next/link";
 import React from "react";
+import {
+  CheckBox as CheckBoxIcon,
+  CheckBoxOutlineBlank as CheckBoxOutlineBlankIcon,
+} from "@material-ui/icons";
 
-const ItemsPage = () => {
+const ItemsPage = ({ items, foundItemIds }) => {
   const [session] = useSession();
 
   if (!session) return <NotLoggedInMessage />;
 
   return (
     <Container align="center">
-      <Typography variant="body1">ITEMS PAGE GOES HERE</Typography>
+      <Typography variant="h3">All Items</Typography>
+      {items &&
+        items.map(({ _id, itemDescription }) => (
+          <Link key={_id} href={`/items/${_id}`}>
+            <StyledButton
+              fullWidth
+              variant="contained"
+              color="secondary"
+              startIcon={
+                foundItemIds.includes(_id) ? (
+                  <CheckBoxIcon />
+                ) : (
+                  <CheckBoxOutlineBlankIcon />
+                )
+              }
+            >
+              {itemDescription}
+            </StyledButton>
+          </Link>
+        ))}
     </Container>
   );
 };
 
 export default ItemsPage;
+
+export const getServerSideProps = async ({ req, res }) => {
+  const session = await getSession({ req });
+  if (session) {
+    await middleware.apply(req, res);
+    const items = await Item.find().lean();
+    const foundItemIds = await Item.where("usersWithItemCollected")
+      .equals(session.user.id)
+      .select("_id");
+    const foundIds = foundItemIds.map((item) => item._id);
+    return {
+      props: {
+        items: JSON.parse(JSON.stringify(items)),
+        foundItemIds: JSON.parse(JSON.stringify(foundIds)),
+      },
+    };
+  } else
+    return {
+      props: {
+        items: [],
+      },
+    };
+};
