3d199e6974a67ed72d7b94c1ff68a2fe24eb9b1d
Claudio Rivera <me@claudiorivera.com>
2020-10-22 14:53:13 -0500

    Fetch user and collection items

--- pages/collections/[userId].js
+++ pages/collections/[userId].js
@@ -1,23 +1,49 @@
+import LargeAvatar from "@components/LargeAvatar";
 import NotLoggedInMessage from "@components/NotLoggedInMessage";
-import StyledButton from "@components/StyledButton";
 import { Container, Typography } from "@material-ui/core";
-import { signIn, useSession } from "next-auth/client";
-import { useRouter } from "next/router";
+import middleware from "@middleware";
+import CollectionItem from "@models/CollectionItem";
+import User from "@models/User";
+import { getSession, useSession } from "next-auth/client";
 import React from "react";
 
-const CollectPage = () => {
-  const router = useRouter();
+const CollectPage = ({ user, items }) => {
   const [session] = useSession();
 
   if (!session) return <NotLoggedInMessage />;
 
   return (
     <Container align="center" maxWidth="xs">
-      <Typography variant="body1">
-        COLLECTION PAGE FOR USER WITH ID: {router.query.userId}
-      </Typography>
+      <LargeAvatar alt={user.name} src={user.image} />
+      <Typography variant="body1">{user.name}</Typography>
     </Container>
   );
 };
 
 export default CollectPage;
+
+export const getServerSideProps = async ({ req, res, params }) => {
+  const session = await getSession({ req });
+  if (session) {
+    await middleware.apply(req, res);
+    const user = await User.findById(params.userId)
+      .select("_id image name")
+      .lean();
+    const items = await CollectionItem.where("user")
+      .equals(params.userId)
+      .select("_id thumbnailUrl item")
+      .populate("item", "itemDescription -_id")
+      .lean();
+    return {
+      props: {
+        user: JSON.parse(JSON.stringify(user)),
+        items: JSON.parse(JSON.stringify(items)),
+      },
+    };
+  } else
+    return {
+      props: {
+        user: null,
+      },
+    };
+};
