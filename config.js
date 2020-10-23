e49591d653636a122057396be9358b3b98d9dba2
Claudio Rivera <me@claudiorivera.com>
2020-10-23 18:58:25 -0500

    Users not admin by default

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const appTitle = "Scavenger Hunt";
 const primaryColor = "#bb4430"; // App bar color
 const secondaryColor = "#dda15e"; // Button color
 const showItemAttribution = false; // Whether to display who added the item under item title
-const newUsersAdminByDefault = true; // Whether to make new users admin by default
+const newUsersAdminByDefault = false; // Whether to make new users admin by default
 const adminLinks = [
   {
     title: "Admin",
