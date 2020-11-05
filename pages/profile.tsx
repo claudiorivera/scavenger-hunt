e79b2996ee32d055f9b02ab901dc555af674cba2
Claudio Rivera <me@claudiorivera.com>
2020-11-05 12:11:15 -0600

    Migrate to TS

---
+++ pages/profile.tsx
@@ -0,0 +1,202 @@
+import LargeAvatar from "@components/LargeAvatar";
+import NotLoggedInMessage from "@components/NotLoggedInMessage";
+import SonicWaiting from "@components/SonicWaiting";
+import StyledButton from "@components/StyledButton";
+import {
+  Badge,
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Input,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import useCurrentUser from "@util/useCurrentUser";
+import Axios from "axios";
+import { useSession } from "next-auth/client";
+import Error from "next/error";
+import { useRouter } from "next/router";
+import React, { FormEvent, SyntheticEvent, useEffect, useState } from "react";
+
+const ProfilePage = () => {
+  const [session] = useSession();
+  const { user } = useCurrentUser();
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [fileInput] = useState("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isUploadingPhoto, setIsUploadingPhoto] = useState(false);
+  const [isSavingChanges, setIsSavingChanges] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setImage(user.image);
+    }
+  }, [user]);
+
+  if (!session) return <NotLoggedInMessage />;
+  if (!user) return null;
+
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleUpdateNameChange = (e: FormEvent) => {
+    setName((e.target as HTMLFormElement).value);
+  };
+
+  const handleFileInputChange = (e: SyntheticEvent<EventTarget>) => {
+    const file = (e.target as HTMLFormElement).files[0];
+    readFile(file);
+  };
+
+  const readFile = (file: Blob) => {
+    setIsUploadingPhoto(true);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      uploadImage(reader.result as string);
+    };
+  };
+
+  const uploadImage = async (base64EncodedImage: string) => {
+    try {
+      // Post to Cloudinary using upload preset for avatars
+      const cloudinaryUploadResponse = await Axios.post(
+        `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/image/upload`,
+        {
+          file: base64EncodedImage,
+          upload_preset:
+            process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET_AVATARS,
+        }
+      );
+      // Create image and thumbnail URLs with proper size/cropping
+      if (cloudinaryUploadResponse.status === 200) {
+        const newAvatarUrl =
+          "https://res.cloudinary.com/" +
+          process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME +
+          "/w_80,h_80,c_fill,g_face,q_auto:best/" +
+          cloudinaryUploadResponse.data.public_id;
+        setIsUploadingPhoto(false);
+        setImage(newAvatarUrl);
+      } else {
+        throw "Sorry, something went wrong while trying to upload";
+      }
+    } catch (error) {
+      return <Error statusCode={500} title={error.message} />;
+    }
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      setIsSavingChanges(true);
+      const response = await Axios.put(`/api/user/${user._id}`, {
+        name,
+        image,
+      });
+      if (response.status === 200) {
+        router.push("/");
+      } else {
+        throw "Unable to update profile";
+      }
+    } catch (error) {
+      return <Error statusCode={500} title={error.message} />;
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <form id="imageUploadForm">
+        <label htmlFor="imagePicker">
+          <Input
+            id="imagePicker"
+            style={{ display: "none" }}
+            name="imagePicker"
+            type="file"
+            inputProps={{ accept: "image/*" }}
+            onChange={handleFileInputChange}
+            value={fileInput}
+          />
+          <Box display="flex" justifyContent="center">
+            <span style={{ cursor: "pointer" }}>
+              <Badge
+                overlap="circle"
+                color="primary"
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                badgeContent={"Edit"}
+              >
+                {isUploadingPhoto ? (
+                  <CircularProgress />
+                ) : (
+                  <LargeAvatar alt={name} src={image} />
+                )}
+              </Badge>
+            </span>
+          </Box>
+        </label>
+      </form>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        onClick={handleDialogOpen}
+      >
+        <Typography style={{ cursor: "pointer" }} variant="h5">
+          {name}
+        </Typography>
+        <EditIcon
+          style={{ cursor: "pointer" }}
+          fontSize="small"
+          color="primary"
+        />
+      </Box>
+      <Dialog
+        open={isDialogOpen}
+        onClose={handleDialogClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Enter New Name</DialogTitle>
+        <DialogContent>
+          <TextField
+            defaultValue={name}
+            autoFocus
+            margin="none"
+            id="name"
+            type="text"
+            fullWidth
+            onChange={handleUpdateNameChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="primary">
+            Continue
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <StyledButton
+        size="large"
+        fullWidth
+        color="secondary"
+        variant="contained"
+        onClick={handleSaveChanges}
+        disabled={isSavingChanges}
+      >
+        {isSavingChanges ? <SonicWaiting /> : "Save Changes"}
+      </StyledButton>
+    </Container>
+  );
+};
+
+export default ProfilePage;
