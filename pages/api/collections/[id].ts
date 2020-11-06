f68a170000531741582a846194c65401f5bbe2f3
Claudio Rivera <me@claudiorivera.com>
2020-11-06 17:01:00 -0600

    Add endpoint for deleting collection item

---
+++ pages/api/collections/[id].ts
@@ -0,0 +1,53 @@
+import middleware from "@middleware";
+import CollectionItem from "@models/CollectionItem";
+import Item from "@models/Item";
+import User from "@models/User";
+import { Types } from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import nextConnect from "next-connect";
+import { ICollectionItem, IItem, IUser } from "types";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+// GET api/collections/id
+// Returns the collection item
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req });
+    if (!session) throw new Error("User not logged in");
+    const item = await CollectionItem.findById(req.query.id).lean();
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "Item not found",
+    });
+  }
+});
+
+// DELETE api/collections/id
+// Deletes the collection item
+handler.delete(async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req });
+    if (!session) throw new Error("User not logged in");
+    const item: ICollectionItem = await CollectionItem.findById(req.query.id);
+    const collectionId: Types.ObjectId = item._id;
+    const originalItem: IItem = await Item.findById(item.item);
+    const user: IUser = await User.findById(item.user);
+    originalItem.usersWhoCollected.pull(user._id);
+    user.itemsCollected.pull(collectionId);
+    await originalItem.save();
+    await user.save();
+    await item.remove();
+    res.json({ message: "Successfully deleted collection item" });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "Item not found",
+    });
+  }
+});
+
+export default handler;
