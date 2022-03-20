8b0fed33c3725ef944614340183abbf96e19652f
Claudio Rivera <me@claudiorivera.com>
2022-03-20 13:37:29 -0500

    ⚰️ Remove unused files

--- components/shared/SonicWaiting.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import sonic from "public/sonic.gif";
-import React from "react";
-
-const SonicWaiting = () => <Image src={sonic} alt="Loading" />;
-
-export default SonicWaiting;
