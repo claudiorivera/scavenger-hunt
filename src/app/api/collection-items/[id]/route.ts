e86f77f5fd27bd6c402f0583381b3a75f1a7ecc5
Claudio Rivera <me@claudiorivera.com>
2023-07-09 21:33:25 -0500

    🚚 Move /api/collection-items/:id to app router

---
+++ src/app/api/collection-items/[id]/route.ts
@@ -0,0 +1,42 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { getUserBySession } from "~/lib/getUserBySession";
+import prisma from "~/lib/prisma";
+import { authOptions } from "~/pages/api/auth/[...nextauth]";
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    const user = await getUserBySession(session);
+
+    if (!user?.isAdmin)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    const deletedCollectionItem = await prisma.collectionItem.delete({
+      where: {
+        id: params.id,
+      },
+    });
+
+    return NextResponse.json(deletedCollectionItem, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message:
+          error instanceof Error
+            ? error.message
+            : "Unable to add item to collection",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
