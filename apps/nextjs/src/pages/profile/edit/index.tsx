2f3028d8345bf42b7ea8e2e9aa6e13498ceb8665
Claudio Rivera <me@claudiorivera.com>
2023-07-16 05:46:08 -0500

    ✨ Finish profile page

---
+++ apps/nextjs/src/pages/profile/edit/index.tsx
@@ -0,0 +1,122 @@
+import classNames from "classnames";
+import NextImage from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, type ChangeEvent } from "react";
+import { z } from "zod";
+import { Input } from "~/components";
+import { useZodForm } from "~/hooks/useZodForm";
+import { base64FromFile } from "~/lib/fileHelpers";
+import { api } from "~/utils/api";
+
+type Photo = Partial<Pick<HTMLImageElement, "src" | "width" | "height">>;
+
+const editProfileSchema = z.object({
+	name: z.string().optional(),
+	base64: z.string().optional(),
+});
+
+export default function EditProfilePage() {
+	const router = useRouter();
+	const utils = api.useContext();
+	const { data: user } = api.user.me.useQuery();
+
+	const [photo, setPhoto] = useState<Photo>({
+		src: user?.image ?? undefined,
+		height: 100,
+		width: 100,
+	});
+
+	const { mutate: updateProfile, isLoading } = api.user.update.useMutation();
+
+	const { register, setValue, handleSubmit } = useZodForm({
+		schema: editProfileSchema,
+		defaultValues: {
+			name: user?.name ?? undefined,
+		},
+	});
+
+	async function onFileChange(event: ChangeEvent<HTMLInputElement>) {
+		if (!!event.currentTarget.files?.[0]) {
+			const file = event.currentTarget.files[0];
+
+			const image = new Image();
+			image.src = URL.createObjectURL(file);
+			image.onload = () => {
+				setPhoto(image);
+			};
+
+			const base64string = await base64FromFile(file);
+
+			if (typeof base64string === "string") {
+				setValue("base64", base64string);
+			}
+		}
+	}
+
+	return (
+		<div className="flex flex-col gap-4">
+			<div className="flex flex-col items-center gap-2">
+				<div className="flex flex-col items-center gap-4">
+					<form
+						id="update-contact"
+						onSubmit={handleSubmit((values) => {
+							updateProfile(values, {
+								onSuccess: () => {
+									void utils.user.me.invalidate();
+									router.push("/profile");
+								},
+							});
+						})}
+						className="flex flex-col gap-2"
+					>
+						<div className="mx-auto">
+							<input hidden {...register("base64")} />
+							<div className="placeholder avatar">
+								<div className="bg-base-300 text-base-content ring-secondary relative w-24 rounded-full ring">
+									{!!photo?.src && (
+										<NextImage
+											src={photo.src}
+											alt="avatar"
+											height={photo.height}
+											width={photo.width}
+										/>
+									)}
+									<div className="absolute bottom-0 left-0 right-0 top-0">
+										<div className="flex h-full flex-col justify-end">
+											<label className="hover:bg-base-100/90 cursor-pointer text-center">
+												<div className="hover:text-secondary text-transparent">
+													Edit
+												</div>
+												<input
+													className="hidden"
+													type="file"
+													accept="image/*"
+													onChange={onFileChange}
+												/>
+											</label>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<Input
+							{...register("name")}
+							autoComplete="name"
+							placeholder="My Name"
+						/>
+						<button
+							type="submit"
+							className={classNames("btn btn-secondary", {
+								loading: isLoading,
+							})}
+							disabled={isLoading}
+						>
+							Save Changes
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,5 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+const updateProfileSchema = z.object({
+	base64: z.string().optional(),
+	name: z.string().optional(),
+});
+
+async function uploadPhoto({
+	base64,
+	userId,
+}: {
+	base64: string;
+	userId: string;
+}) {
+	try {
+		const { secure_url, height, width } = await cloudinary.uploader.upload(
+			base64,
+			{
+				public_id: `${userId}`,
+				folder: "scavenger-hunt/profile-photos",
+			},
+		);
+
+		return { url: secure_url, height, width };
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
 
 export const userRouter = createTRPCRouter({
 	me: protectedProcedure.query(({ ctx }) => {
@@ -34,6 +63,28 @@ export const userRouter = createTRPCRouter({
 			return ctx.prisma.user.delete({
 				where: {
 					id: input,
+				},
+			});
+		}),
+	update: protectedProcedure
+		.input(updateProfileSchema)
+		.mutation(async ({ ctx, input }) => {
+			let imageUrl;
+			if (input.base64) {
+				const { url } = await uploadPhoto({
+					base64: input.base64,
+					userId: ctx.session.user.id,
+				});
+				imageUrl = url;
+			}
+
+			return ctx.prisma.user.update({
+				where: {
+					id: ctx.session.user.id,
+				},
+				data: {
+					image: imageUrl,
+					name: input.name,
 				},
 			});
 		}),
