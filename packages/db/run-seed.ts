3a0e316d2770a9d6c43deae1aed543445d31f4e8
Claudio Rivera <me@claudiorivera.com>
2023-07-22 18:04:34 -0500

    🌱 Add seed script and cron handler

---
+++ packages/db/run-seed.ts
@@ -0,0 +1,11 @@
+import { prisma, seed } from "index";
+
+seed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
