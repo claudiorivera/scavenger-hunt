ba7ff443bfc4046867b06d792f444a5b1e529323
Claudio Rivera <me@claudiorivera.com>
2023-07-22 18:11:53 -0500

    🚧 Ensure all client components use the use client directive

--- apps/nextjs/src/hooks/useItemDetails.tsx
+++ apps/nextjs/src/hooks/useItemDetails.tsx
@@ -1,27 +1,29 @@
+"use client";
+
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
 
 export function useItemDetails({
-	id,
-	currentUser,
+  id,
+  currentUser,
 }: {
-	id: string;
-	currentUser?: RouterOutputs["users"]["me"];
+  id: string;
+  currentUser?: RouterOutputs["users"]["me"];
 }) {
-	const { data: users = [], isLoading: isLoadingUsers } =
-		api.users.withItemIdInCollection.useQuery(id);
-	const { data: item, isLoading: isLoadingItem } = api.items.byId.useQuery(id);
+  const { data: users = [], isLoading: isLoadingUsers } =
+    api.users.withItemIdInCollection.useQuery(id);
+  const { data: item, isLoading: isLoadingItem } = api.items.byId.useQuery(id);
 
-	const isUncollectedByCurrentUser = !users.some(
-		(user) => !!currentUser?.email && user.email === currentUser.email,
-	);
+  const isUncollectedByCurrentUser = !users.some(
+    (user) => !!currentUser?.email && user.email === currentUser.email,
+  );
 
-	const isLoading = isLoadingUsers || isLoadingItem;
+  const isLoading = isLoadingUsers || isLoadingItem;
 
-	return {
-		users,
-		item,
-		isUncollectedByCurrentUser,
-		isLoading,
-	};
+  return {
+    users,
+    item,
+    isUncollectedByCurrentUser,
+    isLoading,
+  };
 }
--- apps/nextjs/src/hooks/useZodForm.tsx
+++ apps/nextjs/src/hooks/useZodForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useId } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { FieldValues, UseFormProps, UseFormReturn } from "react-hook-form";
@@ -5,29 +7,29 @@ import { useForm, useFormContext } from "react-hook-form";
 import type { z } from "zod";
 
 type UseZodForm<TInput extends FieldValues> = UseFormReturn<TInput> & {
-	/**
-	 * A unique ID for this form.
-	 */
-	id: string;
+  /**
+   * A unique ID for this form.
+   */
+  id: string;
 };
 export function useZodForm<TSchema extends z.ZodType>(
-	props: Omit<UseFormProps<TSchema["_input"]>, "resolver"> & {
-		schema: TSchema;
-	},
+  props: Omit<UseFormProps<TSchema["_input"]>, "resolver"> & {
+    schema: TSchema;
+  },
 ) {
-	const form = useForm<TSchema["_input"]>({
-		...props,
-		resolver: zodResolver(props.schema, undefined, {
-			// This makes it so we can use `.transform()`s on the schema without same transform getting applied again when it reaches the server
-			raw: true,
-		}),
-	}) as UseZodForm<TSchema["_input"]>;
+  const form = useForm<TSchema["_input"]>({
+    ...props,
+    resolver: zodResolver(props.schema, undefined, {
+      // This makes it so we can use `.transform()`s on the schema without same transform getting applied again when it reaches the server
+      raw: true,
+    }),
+  }) as UseZodForm<TSchema["_input"]>;
 
-	form.id = useId();
+  form.id = useId();
 
-	return form;
+  return form;
 }
 
 export function useZodFormContext<TSchema extends z.ZodType>() {
-	return useFormContext<TSchema["_input"]>();
+  return useFormContext<TSchema["_input"]>();
 }
