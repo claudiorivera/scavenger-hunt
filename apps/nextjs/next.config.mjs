0ed6db5c12c946760b3e21a54a96e5bf54f80fb6
Claudio Rivera <me@claudiorivera.com>
2023-07-22 21:38:20 -0500

    ⏪ Revert removing env import

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,28 +1,29 @@
 // Importing env files here to validate on build
 import "@claudiorivera/auth/env.mjs";
+import "./src/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [
-    "@claudiorivera/api",
-    "@claudiorivera/auth",
-    "@claudiorivera/db",
-  ],
-  /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "claudiorivera.com",
-      "loremflickr.com",
-      "picsum.photos",
-      "avatars.githubusercontent.com",
-      "cloudflare-ipfs.com",
-    ],
-  },
+	reactStrictMode: true,
+	/** Enables hot reloading for local packages without a build step */
+	transpilePackages: [
+		"@claudiorivera/api",
+		"@claudiorivera/auth",
+		"@claudiorivera/db",
+	],
+	/** We already do linting and typechecking as separate tasks in CI */
+	eslint: { ignoreDuringBuilds: true },
+	typescript: { ignoreBuildErrors: true },
+	images: {
+		domains: [
+			"res.cloudinary.com",
+			"claudiorivera.com",
+			"loremflickr.com",
+			"picsum.photos",
+			"avatars.githubusercontent.com",
+			"cloudflare-ipfs.com",
+		],
+	},
 };
 
 export default config;
