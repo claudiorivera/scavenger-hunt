fae15f3357485cc1455715c2b499ef60666ad351
Claudio Rivera <me@claudiorivera.com>
2023-08-15 18:05:07 -0500

    🚧 Remove redundant header and implement flash list's header prop

--- apps/expo/src/app/items/index.tsx
+++ apps/expo/src/app/items/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native";
 import { Link } from "expo-router";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FlashList } from "@shopify/flash-list";
+import colors from "tailwindcss/colors";
 
 import { api } from "~/utils/api";
 
@@ -12,37 +13,39 @@ export default function Items() {
 	const totalItems = uncollectedItems.length + collectedItems.length;
 
 	return (
-		<View className="flex h-full flex-col items-center gap-4 p-4">
-			<Text className="text-5xl">All Items</Text>
-			<View>
-				<Text>
-					Found {collectedItems.length} out of {totalItems} items! 📷
-				</Text>
-			</View>
-			<View className="h-full w-full">
-				<FlashList
-					data={uncollectedItems}
-					renderItem={({ item }) => (
-						<View className="flex flex-row items-center gap-2">
+		<View className="h-full w-full p-4">
+			<FlashList
+				ListHeaderComponent={() => (
+					<View className="mx-auto mb-4">
+						<Text>
+							Found {collectedItems.length} out of {totalItems} items! 📷
+						</Text>
+					</View>
+				)}
+				data={uncollectedItems}
+				renderItem={({ item }) => (
+					<View className="flex flex-row items-center gap-2">
+						<View className="flex-1 rounded-md bg-yellow-500 p-4">
 							<Link
-								className="flex-1 rounded bg-yellow-500 p-2 text-center font-semibold uppercase text-white"
+								className="text-center font-semibold uppercase text-black"
 								href={`/items/${item.id}`}
 							>
 								{item.description}
 							</Link>
-							<View>
-								<MaterialCommunityIcons
-									name="minus-circle-outline"
-									size={24}
-									color="black"
-								/>
-							</View>
 						</View>
-					)}
-					ItemSeparatorComponent={() => <View className="h-2" />}
-					estimatedItemSize={50}
-				/>
-			</View>
+						<View>
+							<MaterialCommunityIcons
+								name="minus-circle-outline"
+								size={24}
+								color={colors.yellow[500]}
+							/>
+						</View>
+					</View>
+				)}
+				ItemSeparatorComponent={() => <View className="h-2" />}
+				estimatedItemSize={50}
+				showsVerticalScrollIndicator={false}
+			/>
 		</View>
 	);
 }
