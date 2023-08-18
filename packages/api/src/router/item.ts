9b09f83e09232e18e8822d731badcfd62d9ef3ee
Claudio Rivera <me@claudiorivera.com>
2023-08-18 17:41:49 -0500

    🔥 Remove unused procedure

--- packages/api/src/router/item.ts
+++ packages/api/src/router/item.ts
@@ -13,14 +13,14 @@ import {
 const defaultItemSelect = Prisma.validator<Prisma.ItemSelect>()({
 	id: true,
 	description: true,
-})
+});
 
 export const itemRouter = createTRPCRouter({
-	all: publicProcedure.query(({ ctx }) => {
-		return ctx.prisma.item.findMany({ orderBy: { id: "desc" } });
-	}),
 	byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-		return ctx.prisma.item.findUnique({ where: { id: input }, select: defaultItemSelect });
+		return ctx.prisma.item.findUnique({
+			where: { id: input },
+			select: defaultItemSelect,
+		});
 	}),
 	collected: protectedProcedure.query(({ ctx }) => {
 		return ctx.prisma.item.findMany({
