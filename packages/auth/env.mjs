02b4a2f20f6a8c688b0b3ab4aae1ca4729454ed8
Claudio Rivera <me@claudiorivera.com>
2024-02-06 17:57:19 -0600

    🔥 Remove env vars

--- packages/auth/env.mjs
+++ packages/auth/env.mjs
@@ -9,22 +9,12 @@ export const env = createEnv({
 			process.env.NODE_ENV === "production"
 				? z.string()
 				: z.string().optional(),
-		AUTH_URL: z.preprocess(
-			// This makes Vercel deployments not fail if you don't set AUTH_URL
-			// Since NextAuth.js automatically uses the VERCEL_URL if present.
-			(str) => process.env.VERCEL_URL ?? str,
-			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
-			process.env.VERCEL ? z.string() : z.string().url(),
-		),
-		VERCEL_ENV: z.enum(["production", "preview", "development"]),
 	},
 	client: {},
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
-		AUTH_URL: process.env.AUTH_URL,
 		AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
 		AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
-		VERCEL_ENV: process.env.VERCEL_ENV,
 	},
 	emptyStringAsUndefined: true,
 	skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
