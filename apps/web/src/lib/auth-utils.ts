27326d4e818285252fa2eddb9a1f52b56881bcfe
Claudio Rivera <me@claudiorivera.com>
2025-01-11 22:29:21 -0600

    🐛 Fix signin route

--- apps/web/src/lib/auth-utils.ts
+++ apps/web/src/lib/auth-utils.ts
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 export async function getSessionOrThrow() {
 	const session = await auth();
 
-	if (!session) redirect("/sign-in");
+	if (!session) redirect("/api/auth/signin");
 
 	return session;
 }
