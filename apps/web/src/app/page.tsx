4814ff0f2e1500a788572a608c3f6252f31a45ca
Claudio Rivera <me@claudiorivera.com>
2025-01-16 20:50:54 -0600

    💄 Update hunt list styles

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -13,50 +13,60 @@ export default async function HomePage() {
 	const availableHunts = await getAvailableHunts();
 
 	return (
-		<div className="flex flex-col items-center gap-4">
-			<Avatar className="h-24 w-24">
-				<AvatarImage src={session.user.image ?? undefined} alt="User Avatar" />
-				<AvatarFallback>{getInitials(session.user?.name)}</AvatarFallback>
-			</Avatar>
+		<div className="flex flex-col justify-center gap-4">
+			<div className="flex justify-center">
+				<Avatar className="h-24 w-24">
+					<AvatarImage
+						src={session.user.image ?? undefined}
+						alt="User Avatar"
+					/>
+					<AvatarFallback>{getInitials(session.user?.name)}</AvatarFallback>
+				</Avatar>
+			</div>
 
 			<header className="font-semibold text-2xl leading-snug">
 				{session.user?.name ?? "Anonymous User"}
 			</header>
 
-			<div>
-				<h2>My Active Hunts</h2>
-				<ul className="flex w-full flex-col gap-4">
-					{myParticipations.map((participation) => (
-						<li key={participation.id}>
-							<Button variant="secondary" asChild>
-								<Link href={`/hunts/${participation.huntId}`}>
-									{participation.hunt.createdBy.name}'s Hunt
-								</Link>
-							</Button>
-						</li>
-					))}
-				</ul>
-			</div>
+			{!!myParticipations.length && (
+				<>
+					<h2 className="font-semibold text-lg">My Active Hunts</h2>
+					<ul className="flex flex-col gap-4">
+						{myParticipations.map((participation) => (
+							<li key={participation.id}>
+								<Button variant="secondary" asChild className="w-full">
+									<Link href={`/hunts/${participation.huntId}`}>
+										{participation.hunt.createdBy.name}'s Hunt
+									</Link>
+								</Button>
+							</li>
+						))}
+					</ul>
+				</>
+			)}
 
 			{!!availableHunts.length && (
-				<div>
-					<h2>Available Hunts</h2>
-					<ul className="flex w-full flex-col gap-4">
+				<>
+					<h2 className="font-semibold text-lg">Available Hunts</h2>
+					<ul className="flex flex-col gap-4">
 						{availableHunts.map((hunt) => (
-							<li key={hunt.id} className="flex items-center gap-4">
+							<li
+								key={hunt.id}
+								className="flex items-center justify-between gap-4"
+							>
 								<p>{hunt.createdBy.name}'s Hunt</p>
 								<JoinButton huntId={hunt.id} />
 							</li>
 						))}
 					</ul>
-				</div>
+				</>
 			)}
 
-			<div>
-				<Button variant="secondary" asChild>
-					<Link href="/hunts/create">Create a New Hunt</Link>
-				</Button>
-			</div>
+			<div className="-mx-1 my-1 h-px bg-muted" />
+
+			<Button variant="secondary" asChild>
+				<Link href="/hunts/create">Create a New Hunt</Link>
+			</Button>
 		</div>
 	);
 }
