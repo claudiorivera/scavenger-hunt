e2d0caaeee42e9d4b158ccbfbe87d26031142772
Claudio Rivera <me@claudiorivera.com>
2026-03-14 22:11:04 -0500

    ⬆️ Bump deps

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -171,11 +171,11 @@ function CommandShortcut({
 export {
 	Command,
 	CommandDialog,
-	CommandInput,
-	CommandList,
 	CommandEmpty,
 	CommandGroup,
+	CommandInput,
 	CommandItem,
-	CommandShortcut,
+	CommandList,
 	CommandSeparator,
+	CommandShortcut,
 };
